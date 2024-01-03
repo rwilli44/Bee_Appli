09402be0a721514f0d4110c139bfa7334834cb7a
Rachel W. <rachelwilliams@mailbox.org>
2024-01-03 23:50:27 +0100

    added required installations to INSTALLED_APPS

--- bee_appli/bee_appli/settings.py
+++ bee_appli/bee_appli/settings.py
@@ -36,12 +36,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Installed apps
+    "axes",
+    "admin_honeypot",
+    "debug_toolbar",
+    "django_filters",
+    "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
