325ef4791440e93f897bb925b319ac0713e3a992
Rachel W. <rachelwilliams@mailbox.org>
2024-01-08 15:08:56 +0100

    edited IsKeeper to correct errors

--- bee_appli/apiary/localpermissions.py
+++ bee_appli/apiary/localpermissions.py
@@ -2,11 +2,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 # Local imports
-from .models import BeeYard
+from .models import BeeYard, Hive, Intervention
 
 
 class IsKeeperOrReadOnly(BasePermission):
-    """Used to ensure only an animal's keeper can edit its data."""
+    """Used to ensure only a beeyard's beekeeper can edit its data,
+    but anyone can read it."""
 
     def has_object_permission(self, request, view, obj):
         # allow read-only
@@ -15,4 +16,18 @@ class IsKeeperOrReadOnly(BasePermission):
 
         if obj.beekeeper == request.user:
             return True
+        return False
+
+
+class IsKeeper(BasePermission):
+    """Used to ensure only a beeyard's beekeeper can edit its data."""
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, BeeYard):
+            return obj.beekeeper == request.user
+        elif isinstance(obj, Intervention):
+            hive = Hive.objects.get(id=obj.hive_affected_id)
+            return hive.beeyard.beekeeper == request.user
+        elif isinstance(obj, Hive):
+            return obj.beeyard.beekeeper == request.user
         return False
