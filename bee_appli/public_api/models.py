9179ac7808b94405ba391030fc663e0e99da00cc
Rachel W. <rachelwilliams@mailbox.org>
2024-01-08 16:47:32 +0100

    changed publiccontact/user relationship to onetoonefiled

--- bee_appli/public_api/models.py
+++ bee_appli/public_api/models.py
@@ -8,7 +8,7 @@ class PublicContact(models.Model):
     """This model serves to store all Beekeepers who allow their
     contact information to be made public."""
 
-    public_beekeeper_info = models.ForeignKey(
+    public_beekeeper_info = models.OneToOneField(
         User,
         on_delete=CASCADE,
         related_name="allows_public_contact",
