e9bba2d25f29f64c6f885ce57c84a47e565e19a9
Rachel W. <rachelwilliams@mailbox.org>
2024-01-12 23:35:32 +0100

    increased pagination for larger dummy db

--- bee_appli/public_api/paginaton.py
+++ bee_appli/public_api/paginaton.py
@@ -2,7 +2,6 @@ from rest_framework import pagination
 
 
 class PublicAPIPagination(pagination.PageNumberPagination):
-    # pagination is set low because the DB is very small
-    page_size = 5
-    max_page_size = 10
+    page_size = 10
+    max_page_size = 40
     page_size_query_param = "size"
